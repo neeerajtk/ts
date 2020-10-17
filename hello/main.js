02891b6fe28adc17638830cbbbf5378dbd055c18
neeerajtk <neerajtk560@gmail.com>
2020-10-17 23:29:10 +0530

    declaration in ts

--- hello/main.js
+++ hello/main.js
@@ -1,5 +1,10 @@
 "use strict";
 exports.__esModule = true;
-var msg = 'Hello World';
+var msg = 'Welcome';
 console.log(msg);
 // compile using tsc main.ts 
+// tsc main --watch 
+var sum = 10;
+var y = 20;
+var title;
+var help = 'title';
--- hello/main.ts
+++ hello/main.ts
@@ -1,5 +1,13 @@
 export {}
-let msg = 'Hello World';
+let msg = 'Welcome';
 console.log(msg);
 
 // compile using tsc main.ts 
+// tsc main --watch 
+
+let sum = 10;
+const y = 20;
+
+let title;
+
+const help = 'title';
