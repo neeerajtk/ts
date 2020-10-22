57f439c83c6522f7603231e2caba634d7d0c53b3
neeerajtk <neerajtk560@gmail.com>
2020-10-22 10:53:30 +0530

    textfield added

---
+++ first-react/src/TextField.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const TextField: React.FC<{text:string}> = () => {
+    return (
+        <div>
+            Hello 2
+        </div>
+    )
+}
+export default TextField;
